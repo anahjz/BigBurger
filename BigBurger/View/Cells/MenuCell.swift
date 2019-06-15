993ed2e7b27cbe305fdd827903652b7af2c9bfed
AnaHjz <ana.hedjazi68@gmail.com>
2019-06-15 16:03:43 +0430

    Set menu detail to menu cell.

--- BigBurger/View/Cells/MenuCell.swift
+++ BigBurger/View/Cells/MenuCell.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 class MenuCell: UITableViewCell {
     
@@ -16,21 +17,54 @@ class MenuCell: UITableViewCell {
     @IBOutlet weak var foodDescription: UILabel!
     @IBOutlet weak var foodPrice: UILabel!
     
-    
     var food: Food? {
         didSet {
-
+            updateUI()
         }
     }
     
+    
     override func awakeFromNib() {
         
+        
+    }
+    override func layoutSubviews() {
     }
     
     private func updateUI(){
-        func updateFoodImage(){}
-        func updateFoodName(){}
-        func updateFoodDescription(){}
-        func updateFoodPrice(){}
+        
+        func updateFoodImage(){
+            guard let image = food?.image else {
+                return self.foodImage.image = UIImage(named: "placeholder" )
+            }
+            let imageUrl = URL(string: image)
+            self.foodImage.sd_setImage(with: imageUrl, placeholderImage: UIImage(named: "placeholder"))
+        }
+        
+        func updateFoodName() {
+            guard let name = food?.title else {
+                return self.foodName.text = "title is empty"
+            }
+            self.foodName.text = name
+        }
+        
+        func updateFoodDescription() {
+            guard let desc = food?.description else {
+                return self.foodDescription.text = "description is empty"
+            }
+            self.foodDescription.text = desc
+        }
+        
+        func updateFoodPrice() {
+            guard let price = food?.priceInLira() else {
+                return self.foodPrice.text = "0.0"
+            }
+            self.foodPrice.text = price
+        }
+        
+        updateFoodImage()
+        updateFoodName()
+        updateFoodDescription()
+        updateFoodPrice()
     }
 }
